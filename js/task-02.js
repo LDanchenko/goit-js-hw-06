940abbedd6bdd07311962db6f98b3223c58622c5
Lubov <2l-u-b-o-v@ukr.net>
2022-01-11 15:04:16 +0200

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listItems = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  return listItem;
+});
+
+const listIngredients = document.querySelector('#ingredients');
+
+listIngredients.append(...listItems);
