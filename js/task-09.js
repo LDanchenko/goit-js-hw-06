b6fc98c642b78c5e582ce5cab7ee38a6e53baa9f
Lubov <2l-u-b-o-v@ukr.net>
2022-01-14 13:52:52 +0200

    tadk09 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const changeColorButton = document.querySelector('.change-color');
+const outputSpan = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  outputSpan.textContent = document.body.style.backgroundColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
