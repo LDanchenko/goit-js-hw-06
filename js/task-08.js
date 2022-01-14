e536e62e38828d378c07752c594130dbb4cff61d
Lubov <2l-u-b-o-v@ukr.net>
2022-01-14 13:29:01 +0200

    task08-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { password, email } = event.currentTarget.elements;
+  if (password.value && email.value) {
+    const userData = {
+      password: password.value,
+      email: email.value,
+    };
+    console.log(userData);
+  } else {
+    alert('Please note that all fields are required.');
+  }
+  event.currentTarget.reset();
+}
