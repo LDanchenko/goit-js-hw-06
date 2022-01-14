c5947f7dea29b3fbb163a2a05c86adf5d9456e09
Lubov <2l-u-b-o-v@ukr.net>
2022-01-14 14:24:35 +0200

    task10 - done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const overlayDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', () => {
+  const inputAmount = document.querySelector('#controls > input');
+  if (!inputAmount.value) {
+    alert('Empty counter');
+  } else {
+    destroyBoxes();
+    overlayDiv.append(...createBoxes(inputAmount.value));
+  }
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  const divArray = [];
+  const height = 30;
+  const width = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.height = `${height + i * 10}px`;
+    div.style.width = `${width + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    divArray.push(div);
+  }
+  return divArray;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes() {
+  overlayDiv.innerHTML = '';
 }
